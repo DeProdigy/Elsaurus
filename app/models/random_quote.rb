00a3f424ab3cf05bce0e9b512eeb11e6cc7586b8
DeProdigy <alex.hint.ah@gmail.com>
2014-11-30 17:37:04 -0500

    Restructure models and add more data.

--- app/models/random_quote.rb
+++ app/models/random_quote.rb
@@ -1,10 +1,39 @@
 class RandomQuote
   def render
+    all_quotes.sample
+  end
+
+  private
+
+  def all_quotes
     [
-        'I pee',
-        'I see',
-        'I can',
-        'I do'
-    ].sample
+        'That gave me goosebumps on my ankles.',
+        'Make some breakfast & groove.',
+        "Take 3 seconds to enjoy your breath and really dig where you're at right now.",
+        'write. more. letters. share. your. love.',
+        "Brooklyn, I'm coming.",
+        'Life is brilliant.',
+        'Some people feel the rain, while others just get wet.',
+        'The moment you cease to see this life as a journey is the moment you begin to die.',
+        'This moment is poetry.',
+        'Hold each other kindly and in love, for when you find yourself facing death, that is all that may matter.',
+        "As the poet, Christopher Wallace, once said, 'Spread love, it's the Brooklyn way.",
+        'Damn grateful to be alive.',
+        'Touch a life, any life, in a good way, and your life will be touched in the best way.',
+        "This life's a trip.",
+        "57 years later and I'll still be slapping Shaggy's It Wasn't Me. thuglife.",
+        'All I want for christmas is a hebrew girl with a kwanzaa booty.',
+        'This saving the world thing entails some real long nights.',
+        'A damn good day to be alive, happy monday fam.',
+        '"Painting is silent poetry, and poetry is painting that speaks.” And sex is all that combined, if you let it be.',
+        "Cheesy as hell but I'm cheesin' as El.",
+        "And if we can't run, we'll fly.",
+        'You find that time is ephemeral - age is nothing - so you live without apology or regret.',
+        "Don't ever underestimate the impact you can have on another person's life.",
+        "It doesn't have to be perfect. It just has to be. And that's perfect.",
+        'Make some breakfast & groove.',
+        'Hold up let me grab my fuzzy pink robe.',
+        'Hakuna Matata.'
+    ]
   end
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -6,14 +6,56 @@ class Translation
   end
 
   def prepends
-    ['Yes, yes!', 'Yo, yo!', 'Fly people,', 'Alright, alright.', 'Gotta love it.', 'Word.', 'The first azerbaijani brother to'].sample
+    all_prepeds.sample
   end
 
   def appends
-    ['wordwordword.', '#brooklyn', '#justakidfrombrooklyn', '#damn', "It's so good to be a gangster.", 'We out son.', 'word.', '‪#‎keeponboogieing‬', '#boogie', 'stay classy.', 'Brooklyn, get ready.', 'awwwshit', 'Papa Bear.'].sample
+    all_appends.sample
   end
 
   def render
+    # get rid off empty spaces and check if still empty
     text.gsub(/\s+/, '').empty? ? RandomQuote.new.render : prepends + ' ' + text + '. ' + appends
+  end
+
+  private
+
+  def all_prepeds
+    ['Yes, yes!',
+     'Yo, yo!',
+     'Fly people,',
+     'Alright, alright.',
+     'Gotta love it.',
+     'Word.',
+     'The first azerbaijani brother to',
+     'Oh fasho.'
+    ]
+  end
+
+  def all_appends
+    ['wordwordword.',
+     '#brooklyn',
+     '#justakidfrombrooklyn',
+     '#damn',
+     "It's so good to be a gangster.",
+     'We out son.',
+     'word.',
+     '‪#‎keeponboogieing‬',
+     '#boogie',
+     'Stay classy.',
+     'Brooklyn, get ready.',
+     'awwwshit',
+     'Papa Bear.',
+     'Hakuna Matata.',
+     'Hotsauce.',
+     'Vale, vale.',
+     'Hold up let me grab my fuzzy pink robe.',
+     'The brother still got it.',
+     'This track slaps so hard.',
+     'mate.',
+     'On some gangster shit.',
+     'Cmon B!',
+     'Got this son.'
+    ]
   end
 end
